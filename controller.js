1f1faab7b4620545556a5e740637f9c12e89ddff
WellDone88 <kenneth.moren@live.no>
2025-09-25 12:19:54 +0200

    sadas

---
+++ controller.js
@@ -0,0 +1,11 @@
+
+
+while (i <= 10){
+   console.log(i);
+   i++;
+}
+
+function RandomNumbersAreEqual(){
+    Math.floor(Math.random())
+    console.log()
+}
---
+++ view.js
@@ -0,0 +1,21 @@
+
+
+function UpdateView(){
+    let HTML = document.getElementById("app").innerHTML = /*HTML*/ `
+    <p id="isTheNumbersEqulal"></p>
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    `
+document.getElementById('isTheNumbersEqulal').innerHTML;
+}
+
+ UpdateView()
