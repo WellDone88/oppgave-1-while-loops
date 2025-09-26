7c944f6917bb7db62057ea2e4c43d38da4b98f6e
WellDone88 <kenneth.moren@live.no>
2025-09-26 14:22:48 +0200

    smått

--- controller.js
+++ controller.js
@@ -1,11 +1,16 @@
 
 
-while (i <= 10){
-   console.log(i);
+while (i <= 100){
+  console.log(i);
    i++;
 }
 
 function RandomNumbersAreEqual(){
-    Math.floor(Math.random())
-    console.log()
+    let num1 = TwoRandomNumbers[Math.floor(Math.random() * TwoRandomNumbers.length + 1)]
+    let num2 = TwoRandomNumbers[Math.floor(Math.random() * TwoRandomNumbers.length + 1)]
+
+    if(num1 == num2)
+        document.getElementById("isTheNumbersEqulal").innerHTML += num1 +'<br>' + num2 + '<br>';
+    console.log(num1, num2)
+    
 }
--- view.js
+++ view.js
@@ -3,6 +3,7 @@
 function UpdateView(){
     let HTML = document.getElementById("app").innerHTML = /*HTML*/ `
     <p id="isTheNumbersEqulal"></p>
+    <button onclick="RandomNumbersAreEqual()">Klikk</button>
     
     
     
